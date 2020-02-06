0d6183b2b452c4836b53b58368ef5d6420b2bd0c
HyperAutomation <58214974+HyperAutomation@users.noreply.github.com>
2020-02-06 11:26:08 +0000

    Create OSDriverTest.java

---
+++ src/test/java/com/microsoft/appcenter/appium/OSDriverTest.java
@@ -0,0 +1,62 @@
+/**
+ * 
+ */
+package com.microsoft.appcenter.appium;
+
+import org.junit.*;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * @author JonathonWright
+ *
+ */
+public class OSDriverTest {
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		// Null
+	}
+
+}
